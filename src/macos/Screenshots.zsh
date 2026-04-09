d0a2b5b69a0e4823153d91346c205501b56c182f
Rafael Bicalho <rbmbika@gmail.com>
2026-04-09 11:09:39 -0300

    Update macOS defaults script

--- src/macos/Screenshots.zsh
+++ src/macos/Screenshots.zsh
@@ -2,3 +2,9 @@
 
 # Do not display the thumbnail after taking a screenshot
 defaults write com.apple.screencapture "show-thumbnail" -bool "false"
+
+# Set default screenshot location
+mkdir ~/Screen\ Captures
+defaults write com.apple.screencapture "location" -string "~/Screen Captures"
+
+killall SystemUIServer
